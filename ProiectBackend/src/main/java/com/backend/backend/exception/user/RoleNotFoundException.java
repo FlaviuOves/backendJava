1c15769bb5d2a28137743b104eb5a9c5fb5faeb3
Flaviu Sas <flaviusas@Flavius-MacBook-Pro.local>
2022-06-17 11:00:41 +0300

    deleted project folders

--- ProiectBackend/src/main/java/com/backend/backend/exception/user/RoleNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.backend.backend.exception.user;
-
-public class RoleNotFoundException extends RuntimeException{
-    public RoleNotFoundException(String message)
-    {
-        super(message);
-    }
-}
